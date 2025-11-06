7dcbd6db9ba57878b4156f03d0ad479942363e6f
Kinan Hashoum <kinan.hashoum123@gmail.com>
2025-11-06 12:44:03 +0100

    corected nummbers

--- 01_Basics/Aufgabe_50/50_1.js
+++ 01_Basics/Aufgabe_50/50_1.js
@@ -1,15 +1,15 @@
 var points = Math.floor(Math.random() * 100); 
 
-if (points > 89) {
+if (points > 90) {
     console.log("Grade A");
     
-} else if (points > 79) {
+} else if (points > 80) {
     console.log("Grade B");
 
-} else if (points > 69) {
+} else if (points > 70) {
     console.log("Grade C");
     
-} else if (points > 59) {
+} else if (points > 60) {
     console.log("Grade D");
 
 } else {
