eb481f949f81e017f7ee3bf3d371518adbba75b7
Kinan Hashoum <kinan.hashoum123@gmail.com>
2025-11-06 13:10:30 +0100

    finished 44.2

---
+++ 01_Basics/Aufgabe_44/44_2.js
@@ -0,0 +1,17 @@
+let num = -5;
+
+if (num > 0) {
+    console.log("the number is positive");
+} else {
+    console.log("the number is not positive");
+}
+if (num < 0) {
+    console.log("the number is negative");
+} else {
+    console.log("the number is positive or 0");
+}
+if (num == 0) {
+    console.log("the number is 0");
+} else {
+    console.log("the number is not 0");
+}
